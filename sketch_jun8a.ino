e2fa1a2ba1a88a5f99756002ba482b120be31a61
pentyann32 <hisatopen@gmail.com>
2024-06-11 11:40:39 +0900

    Update sketch_jun8a.ino

--- sketch_jun8a.ino
+++ sketch_jun8a.ino
@@ -4,16 +4,42 @@
 
 Servo kaitenservo;
 Servo armservo;
-int sleep_pin = 2;
 int reset_pin = 30;
-int rightmoter_clock = 9;
-int leftmoter_clock = 10;
+int right_clock = 9;
+int left_clock = 10;
 int right_sensor = A1;
 int left_sensor = A0;
 int right_direction = 8;
 int left_direction = 12;
 int kyuuinnmoter_1 = 52;
 int kyuuinnmoter_2 = 53;
+
+int sikiiti = 300;
+
+void setup(){
+  Serial.begin(9600);
+  TCCR2B = (TCCR2B & 0b00111111) | 0x01;
+  TCCR2A = (TCCR2A & 0b00111111) | 0x01;
+  pinMode(right_direction,OUTPUT);
+  pinMode(left_direction,OUTPUT);
+  pinMode(kyuuinnmoter_1,OUTPUT);
+  pinMode(kyuuinnmoter_2,OUTPUT);
+  pinMode(reset_pin,OUTPUT);
+  pinMode(left_clock,OUTPUT);
+  pinMode(right_clock,OUTPUT);
+  kaitenservo.attach(29);
+  armservo.attach(31);
+  armservo.write(100);
+  kaitenservo.write(90);
+  digitalWrite(reset_pin,HIGH);
+  delay(100);
+  digitalWrite(reset_pin,LOW);
+  analogWrite(rightmoter_clock,127);
+  analogWrite(leftmoter_clock,127);
+  delay(500);
+  if(analogRead(rightsensor)>sikiiti && analogRead(left_sensor)>sikiiti){
+  }
+}
 
 void linetrace(){
   if(analogRead(right_sensor) < 150 && analogRead(left_sensor) >= 150){
@@ -26,16 +52,6 @@ void linetrace(){
     digitalWrite(right_direction,LOW);
     digitalWrite(left_direction,LOW);
   }
-}
-
-void setup(){
-  Serial.begin(9600);
-  kaitenservo.attach(29);
-  armservo.attach(31);
-  pinMode(kyuuinnmoter_1,OUTPUT);
-  pinMode(kyuuinnmoter_2,OUTPUT);
-  delay(100);
-  armservo.write(90);
 }
 
 void loop() {
